abfc66fcf9c29c15765a08ff38b992c6d36e4a0e
Barlus <barluscuda@gmail.com>
2026-08-28 10:50:23 +0700

    add: payment link request

--- payment_link.go
+++ payment_link.go
@@ -1,14 +1,29 @@
 package phajay
 
+// ---- PaymentLink Struct ----
+
 type PaymentLink struct {
-	Request paymentLinkRequest
+	Request  paymentLinkRequest
+	Response paymentLinkResponse
 }
 
 type paymentLinkRequest struct {
 	OrderNo     string `json:"orderNo"`
-	Amount      int    `json:"amount"` // required
+	Amount      int    `json:"amount"`      // required
 	Description string `json:"description"` // required
 	Tag1        string `json:"tag1"`
 	Tag2        string `json:"tag2"`
 	Tag3        string `json:"tag3"`
+}
+
+type paymentLinkResponse struct {
+	Message     string `json:"message"`
+	RedirectURL string `json:"redirectURL"`
+	OrderNo     string `json:"orderNo"`
+}
+
+// ---- PaymentLink Usecase ----
+
+func (p *Phajay) CreatePaymentLink(request paymentLinkRequest) (paymentLinkResponse, error) {
+	return paymentLinkResponse{}, nil
 }
